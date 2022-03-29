571f0c597e979b859be90309690c234a36fdb04a
Nick Lamattina <nicklamattina@outlook.com.au>
2022-03-29 20:37:24 +1100

    Added Pi, power operator and updated readme

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## Introduction
 
-For this project, I aim to create a fully functioning calculator by combining all the skills I have learnt so far in HTML, SCSS/CSS and JavaScript. The calculator will support the following operations: [+, -, /, *, =].
+For this project, I aimed to create a fully functioning calculator by combining all the skills I have learnt so far in HTML, SCSS/CSS and JavaScript. The calculator supports the following operations: [+, -, /, *, =, ^] as well as Pi, negative numbers and decimals.
 
 ## Implementation Breakdown
 
-In order to build a calculator, I will implement the below steps:
+Before building my calculator, I drafted the below implementation outline:
 
 1. Create the base HTML elements, including the main output container, buttons and the general layout.
 2. Use SCSS styling to add colour, button styling, element sizing and calculator layout using flexbox/grid.
@@ -16,6 +16,31 @@ In order to build a calculator, I will implement the below steps:
 6. Implement functions for some operations such as equals and clear.
 7. If time allows, additional operators can be added after completing the MVP.
 
-## Inspiration
+## Inspiration (Android 12 Calculator)
 
 <img src="https://cdn.vox-cdn.com/uploads/chorus_asset/file/22835822/Screen_Shot_2021_09_08_at_4.58.56_PM.png" alt="Android 12 Calculator" title="Calculator Inspiration" width="450"/>
+
+## Requirements
+
+### MVP
+
+-   [x] Number keys 0-9
+-   [x] Operator keys (+, -, &divide;, &times;, =)
+-   [x] Decimals support
+-   [x] Display rendering the current calculation
+-   [x] Does not use the eval() function
+
+### Extra Features
+
+-   [x] Supports negative numbers
+-   [x] Output also logs the previous calculation at the top
+-   [x] Power operator ^
+-   [x] Pi button &pi;
+
+## Future of the Project
+
+If I had more time to develop this program, I would like to implement support for order of operations as well as add bracket keys for further complex functionality.
+
+Ideally, I would have also added more responsiveness to each button in the form of visual feedback to the user. Currently the calculator does not handle scientific notation correctly and I would likely need to overhaul my evaluation function to support this.
+
+Overall, I am happy with the state of my JS web calculator given the one week timeframe we were allotted to complete it. It supports a decent number of operations, negatives and has also checks to prevent incorrect inputs by the user.
--- script.js
+++ script.js
@@ -1,39 +1,43 @@
-// const btnOne = document.querySelector("#btnOne");
-const output = document.querySelector(".calc__output");
+const output = document.querySelector(".calc__output__main");
 const btnNums = document.querySelectorAll(".calc__row__btnNum");
 const btnOps = document.querySelectorAll(".calc__row__btnOps");
 const btnClear = document.querySelector("#btnClear");
 const btnDel = document.querySelector("#btnDel");
 const btnEquals = document.querySelector("#btnEquals");
 const btnDot = document.querySelector("#btnDot");
-// const allBtns = document.querySelectorAll(".calc__row__btn");
+const prevOut = document.querySelector(".calc__output__prevOut");
 let currentOut = "";
-let currentTot = 0;
 let evalArr = [];
 let prevEvalCheck = false;
-// let prevOpCheck = false;
-let currVal;
-let lastVal;
 
-// allBtns.forEach((btn) =>
-//     btn.addEventListener("click", () => {
-//         console.log("success");
-//     }),
-// );
+const dispOutput = () => {
+    // output font size is reduced based on the current length
+    if (currentOut.length > 14) output.style.fontSize = "30px";
+    else if (currentOut.length > 11) output.style.fontSize = "40px";
+    else output.style.fontSize = "50px";
 
-btnDot.addEventListener("click", () => {
-    if (prevEvalCheck) {
-        currentOut = "";
-        prevEvalCheck = false;
+    output.innerHTML = currentOut; // outputs current value to the screen
+};
+
+// Evaluation function
+const myEval = (n, m, operation) => {
+    if (n === "π") n = Math.PI;
+    else n = parseFloat(n);
+    if (m === "π") m = Math.PI;
+    else m = parseFloat(m);
+
+    // for operations followed by a negative sign
+    if (operation[1] === "-") {
+        m = -m; // 2nd value is set to be negative
+        operation = operation.slice(0, -1);
     }
-    evalArr = currentOut.split(/([-+×÷]+)/);
-    currVal = currentOut[currentOut.length - 1];
-    if (currVal === ".") currentOut = currentOut.slice(0, -1);
-    if (evalArr[evalArr.length - 1].includes(".")) return;
-    // console.log("dot", currentOut);
-    currentOut += btnDot.innerHTML;
-    dispOutput();
-});
+
+    if (operation === "+") return n + m;
+    if (operation === "-") return n - m;
+    if (operation === "×") return n * m;
+    if (operation === "÷") return n / m;
+    if (operation === "^") return n ** m;
+};
 
 // Digit buttons
 for (let i = 0; i < btnNums.length; i++) {
@@ -42,81 +46,81 @@ for (let i = 0; i < btnNums.length; i++) {
             currentOut = "";
             prevEvalCheck = false;
         }
-        // if (prevOpCheck) prevOpCheck = false;
         currentOut += btnNums[i].innerHTML;
         dispOutput();
     });
 }
 
-const dispOutput = () => {
-    output.innerHTML = currentOut;
-};
-
-// AC - all clear button
-btnClear.addEventListener("click", () => {
-    currentOut = "";
-    dispOutput();
-});
-
-// Delete key
-btnDel.addEventListener("click", () => {
-    currentOut = currentOut.slice(0, -1);
-    dispOutput();
-});
-
-// Operations buttons
+// Operation buttons
 for (let i = 0; i < btnOps.length; i++) {
     btnOps[i].addEventListener("click", () => {
         if (prevEvalCheck) prevEvalCheck = false; // stops new numbers from clearing the output after an eval and then an operation
-        currVal = currentOut[currentOut.length - 1];
-        lastVal = currentOut[currentOut.length - 2];
 
-        if (/[-]/.test(currVal) && /[+×÷]/.test(lastVal)) {
-            return;
-        } else if (/[-]/.test(currVal) && /[0-9]/.test(lastVal)) {
-            currentOut = currentOut.slice(0, -1);
-            console.log("success 2", lastVal, currVal);
-        } else if (/[+×÷]/.test(currVal) && /[+×÷]/.test(btnOps[i].innerHTML)) {
-            currentOut = currentOut.slice(0, -1);
-            console.log("success 3", lastVal, currVal);
+        let currVal = currentOut[currentOut.length - 1]; // last char in the output
+        let prevVal = currentOut[currentOut.length - 2]; // second last char in the output
+        if (/[-]/.test(currVal) && /[+×÷]/.test(prevVal)) {
+            return; // returns if a negative is after an operation to prevent other operations being added
+        } else if (/[-]/.test(currVal) && /[0-9]/.test(prevVal)) {
+            currentOut = currentOut.slice(0, -1); // last char is removed then later replaced with the pressed operator
+        } else if (/[+×÷^]/.test(currVal) && /[+×÷^]/.test(btnOps[i].innerHTML)) {
+            currentOut = currentOut.slice(0, -1); // prevents two operators being pressed in a row
+        } else if (/[0-9π]/.test(currVal) && btnOps[i].innerHTML === "π") {
+            return; // stops repeated presses of PI or directly after a number (an operation is required between a digit and PI to be evaluated)
         }
 
-        // prevOpCheck = true;
-        // currentTot = currentOut;
         currentOut += btnOps[i].innerHTML;
         dispOutput();
     });
 }
 
+// Equal button
 btnEquals.addEventListener("click", () => {
-    evalArr = currentOut.split(/([-+×÷]+)/); // splits the current eval into an array separated by the operations
+    prevOut.innerHTML = currentOut + "="; // displays the previous calculation at the top
+
+    evalArr = currentOut.split(/([-+×÷^]+)/); // splits the current eval into an array separated by operations
 
     if (evalArr[0] === "") evalArr.splice(0, 1, "0"); // adds a zero to the start of the array for expressions starting with a negative number
 
-    currentTot = evalArr[0];
+    let currentTot = evalArr[0];
     for (let i = 1; i < evalArr.length; i += 2) {
-        currentTot = myEval(parseFloat(currentTot), parseFloat(evalArr[i + 1]), evalArr[i]);
+        // iterates from index 1 which contains an operator then a number in the next index
+        currentTot = myEval(currentTot, evalArr[i + 1], evalArr[i]);
     }
-
-    currentOut = +currentTot.toPrecision(10); // plus at the front converts the result back into a number and removes extra zeros
-    // currentOut = currentTot;
-    prevEvalCheck = true; // indicates that equals has been pressed
+    // plus before the var converts the result to a number which removes extra zeros, then the empty string converts it back to a string
+    currentOut = +currentTot.toPrecision(9) + "";
+    prevEvalCheck = true; // tracks when equals has been pressed
     dispOutput();
 });
 
-const myEval = (n, m, operation) => {
-    if (operation[1] === "-") {
-        m = -m;
-        operation = operation.slice(0, -1);
+// Decimal button
+btnDot.addEventListener("click", () => {
+    if (prevEvalCheck) {
+        currentOut = "";
+        prevEvalCheck = false;
     }
-    if (operation === "+") return n + m;
-    if (operation === "-") return n - m;
-    if (operation === "×") return n * m;
-    if (operation === "÷") return n / m;
-};
+    evalArr = currentOut.split(/([-+×÷^]+)/);
+    if (evalArr[evalArr.length - 1].includes(".")) return; // returns if the current number already contains a decimal
 
+    currentOut += btnDot.innerHTML;
+    dispOutput();
+});
+
+// Delete key
+btnDel.addEventListener("click", () => {
+    currentOut = currentOut.slice(0, -1); // removes the last char from the output string
+    dispOutput();
+});
+
+// All clear (AC) button
+btnClear.addEventListener("click", () => {
+    currentOut = "";
+    prevOut.innerHTML = "";
+    dispOutput();
+});
+
+// Event listener to allow keyboard inputs
 document.addEventListener("keydown", (e) => {
-    // console.log(e.key);
+    // The button index has to be specified since the nodelist is not ordered
     if (e.key === "0") btnNums[9].click();
     if (e.key === "1") btnNums[6].click();
     if (e.key === "2") btnNums[7].click();
@@ -132,13 +136,11 @@ document.addEventListener("keydown", (e) => {
     if (e.key === "Backspace") btnDel.click();
     if (e.key === "Delete") btnClear.click();
 
-    if (e.key === "/") btnOps[0].click();
-    if (e.key === "*") btnOps[1].click();
-    if (e.key === "-") btnOps[2].click();
-    if (e.key === "+") btnOps[3].click();
+    if (e.key === "p") btnOps[0].click(); // PI
+    if (e.key === "^") btnOps[1].click();
+    if (e.key === "/") btnOps[2].click();
+    if (e.key === "*") btnOps[3].click();
+    if (e.key === "-") btnOps[4].click();
+    if (e.key === "+") btnOps[5].click();
     if (e.key === "=" || e.key === "Enter") btnEquals.click();
 });
-
-// bugs
-// -pressing a digit then dot twice and another digit removes the dot
-// -negative numbers sometimes break with keyboard only?
